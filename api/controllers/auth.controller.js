477b8e63b70b030fee063539003d5eaaa521d7fb
Azhar Ahmed <qadriiazhar@gmail.com>
2024-12-07 19:34:06 +0530

    create login function in backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import {errorHandler} from '../utils/error.utils.js'
 import bcryptjs from 'bcryptjs'
-
+import jwt from 'jsonwebtoken'
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -33,3 +33,33 @@ export const signUp = async (req, res, next) => {
     next(error)
   }
 };
+
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //  Validation
+  if (!email || !password) {
+    next(errorHandler(400, "All fields are required!"))
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+     return next(errorHandler(400, "User not found!"))
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword){ 
+      return next(errorHandler(400, "Invalid password"));
+    }
+    
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+
+    const {password: pass, ...rest} = validUser._doc;  
+    res.status(200).cookie('access_token',token, {
+      httpOnly: true
+    }).json(rest); 
+
+  } catch (error) {
+    next(error)
+  }
+}
